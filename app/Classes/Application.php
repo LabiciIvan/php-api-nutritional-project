f86375d30186bc6362763f0bd0a265981082b06f
root <root@DESKTOP-MJ55FAL.localdomain>
2024-06-03 16:31:56 +0300

    initial commit

---
+++ app/Classes/Application.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Classes;
+
+use App\Interfaces\ApplicationInterface;
+
+class Application implements ApplicationInterface
+{
+    public function __construct()
+    {
+        
+    }
+
+    public function run(): void
+    {
+        echo 'start application';
+    }
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Classes\Application;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = new Application();
+
+$app->run();
