0d28487561a102ed98e816b0d6c1b91d95a1002a
LabiciIvan <ioanlabici@gmail.com>
2024-06-26 08:49:27 +0300

    NP-14: Created controller and moved logic from index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ use App\Classes\Application;
 
 use App\Utilities\Json;
 use App\Utilities\Response;
-use App\Classes\CaloriesCalculator;
 use App\Utilities\ErrorLogger;
+use App\Controllers\MacronutrientsController;
 
 require __DIR__ . '/vendor/autoload.php';
 
@@ -17,76 +17,9 @@ $router = new Router();
 
 $router->get('/', [Response::class, 'missingMethod']);
 
-$router->post('/calculate/calories/', function () use($request) {
-    $requestData = $request->getRequestData();
+$router->post('/calculate/calories/', [MacronutrientsController::class, 'calculateCalories']);
 
-    $requestData = Json::fromJson($requestData);
-
-    if ($requestData === null) {
-        Response::sendResponse(Json::toJson(['data' => 'Request is missing required data']), 400);
-    }
-
-    $requiredKeys = array_flip(['weight', 'activityLevel']);
-
-    $missingKeys = array_diff_key($requiredKeys, $requestData);
-
-    $extraKeys = array_diff_key($requestData, $requiredKeys);
-
-    if ($missingKeys) {
-        Response::sendResponse(Json::toJson(['data' => 'Missing keys in request: ' . implode(',', array_keys($missingKeys))]), 403);
-    }
-
-    if ($extraKeys) {
-        Response::sendResponse(Json::toJson(['data' => 'Extra keys in request: ' . implode(',', array_keys($extraKeys))]), 403);
-    }
-
-    $kclCalculator = new CaloriesCalculator();
-
-    $totalCalories = $kclCalculator->calculateTotalCalories($requestData['weight'], $requestData['activityLevel']);
-
-    Response::sendResponse(Json::toJson(['data' => ['kcal' => $totalCalories]]), 200);
-});
-
-$router->post('/calculate/macronutrients/', function () use($request) {
-
-    $requestData = $request->getRequestData();
-
-    $requestData = Json::fromJson($requestData);
-
-    if ($requestData === null) {
-        Response::sendResponse(Json::toJson(['data' => 'Request is missing required data']), 400);
-    }
-
-    $requiredKeys = array_flip(['calories', 'protein', 'carbohydrates', 'fats']);
-
-    $missingKeys = array_diff_key($requiredKeys, $requestData);
-
-    $extraKeys = array_diff_key($requestData, $requiredKeys);
-
-    if ($missingKeys) {
-        Response::sendResponse(Json::toJson(['data' => 'Missing keys in request: ' . implode(',', array_keys($missingKeys))]), 403);
-    }
-
-    if ($extraKeys) {
-        Response::sendResponse(Json::toJson(['data' => 'Extra keys in request: ' . implode(',', array_keys($extraKeys))]), 403);
-    }
-
-    $kclCalculator = new CaloriesCalculator();
-
-    try {
-        $macroDistribution = $kclCalculator->calculateMacroDistribution(
-            $requestData['calories'],
-            $requestData['protein'],
-            $requestData['carbohydrates'],
-            $requestData['fats'],
-        );
-    } catch (Exception $e) {
-        ErrorLogger::logError($e->getMessage(), __DIR__ . '/errors.txt');
-        Response::sendResponse(Json::toJson(['data' => $e->getMessage()]), 400);
-    }
-
-    Response::sendResponse(Json::toJson(['data' => $macroDistribution]), 200);
-});
+$router->post('/calculate/macronutrients/', [MacronutrientsController::class, 'calculateMacronutrients']);
 
 $app = new Application($request, $router);
 
