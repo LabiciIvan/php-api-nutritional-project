51e44e6c54765d186e82627a17de947b11de56b5
LabiciIvan <ioanlabici@gmail.com>
2024-08-02 08:34:36 +0300

    NP-35: Fetch logged in users query in AuthMiddleware::class. #35

--- App/Middlewares/AuthMiddleware.php
+++ App/Middlewares/AuthMiddleware.php
@@ -6,8 +6,9 @@ namespace App\Middlewares;
 
 use App\Utilities\Json;
 use App\Classes\Request;
-use App\Database\NutritionPDO;
 use App\Utilities\Response;
+use App\Database\NutritionPDO;
+use App\Utilities\ErrorLogger;
 
 class AuthMiddleware
 {
@@ -22,17 +23,20 @@ class AuthMiddleware
         $db = NutritionPDO::getInstance();
 
         $queryString = <<<EOS
-            SELECT user_id AS userID FROM login WHERE token = '$bearerToken' AND expire_at > CURDATE();
+            SELECT user_id AS userID, expire_at AS expire
+            FROM login
+            WHERE token = '$bearerToken' AND expire_at > CURDATE();
         EOS;
 
         $result = $db->quickFetch($queryString);
 
         if (!$result) {
-            Response::sendResponse(Json::toJson(['fail' => 'User is not logged in']), 404);
+            Response::sendResponse(Json::toJson(['fail' => 'Authenticate to access this resource']), 404);
         }
 
-        var_dump($result);
+        $loginTrack = "User with ID = {$result[0]['userID']}, TOKEN = $bearerToken and EXPIRE = {$result[0]['expire']}, logged in the application.";
 
-        Response::sendResponse(Json::toJson(['data' => 'Bearer Token received'. $bearerToken]), 200);
+        // Keep track in logs of users which logged in
+        ErrorLogger::logError($loginTrack, __DIR__ . '/../../errors.txt');
     }
 }
